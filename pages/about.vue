9ee5e32dfba91c6e40876f72515f46761e1a1f8d
Mirafzal1307 <usmonjonovmirafzal0713@gmail.com>
2022-08-01 18:31:29 +0500

    Vue Nuxt Crash Course finished

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <h1>About</h1>
+        <p>This is a simple example of a Vue.js component.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, corporis! Pariatur, odit nulla sequi aut reiciendis sapiente excepturi minima, aliquam assumenda itaque repudiandae quod beatae nam cumque. Architecto, dolorem dolor!</p>
+    
+    </section>
+</template>
+
+<script>
+export default {
+    middleware: ['auth'],
+}
+</script>
